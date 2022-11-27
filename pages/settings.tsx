29cd776cb018e2e2940fb734925c690eb358b5bf
Christie Reid <christiereid@protonmail.com>
2022-11-27 17:53:26 +0100

    changes to settings page

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -3,11 +3,11 @@ import useProtectedPage from "../hooks/useProtectedPage";
 export default function Settings() {
 	// const authContext = useProtectedPage();
 	return (
-		<div className="grid grid-cols-3 gap-x-8 gap-y-4 mx-4 text-center sans text-2xl">
+		<div className="w-screen grid grid-cols-3 gap-x-8 gap-y-4 mx-4 text-center sans text-2xl">
 			<h1 className="col-span-3">Profile Pictures</h1>
 			<div className="col-span-3"></div>
-			<h1 className="text-right col-span-1">Age</h1>
-			<div className="col-span-2 text-sm">Age Placeholder</div>
+			<h1 className="text-right col-span-1">Birthday</h1>
+			<div className="col-span-2 text-sm">Birthday Placeholder</div>
 			<h1 className="col-span-3">Age Range of Friends</h1>
 			<div className="col-span-3 text-sm">Slider Placeholder</div>
 			<h1 className="text-right col-span-1">Languages</h1>
