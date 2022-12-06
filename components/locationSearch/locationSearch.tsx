6d25d332ea404a5a3764a07424b690b91062a181
Chirag Singal <chirag.prakriya@gmail.com>
2022-12-06 14:30:58 +0100

    location bar geocoding and reverse geocoding

--- components/locationSearch/locationSearch.tsx
+++ components/locationSearch/locationSearch.tsx
@@ -1,44 +1,48 @@
 import React, { useState } from "react";
 import { Combobox } from "@headlessui/react";
-import { useLocation } from "../../hooks/useLocation";
+import { useGpsLocation, useLocation } from "../../hooks/useLocation";
+import { MapPinIcon } from "@heroicons/react/24/outline";
 
-type LocationSearchProps = {
-  selected: string;
-  setSelected: React.Dispatch<React.SetStateAction<string>>;
-};
+export default function LocationSearch() {
+	const [selected, setSelected] = useState("");
+	const { data: gps } = useGpsLocation();
+	const [query, setQuery] = useState("");
+	const { data } = useLocation(query);
 
-export default function LocationSearch({}: LocationSearchProps) {
-  const [selected, setSelected] = useState("");
-  const [query, setQuery] = useState("");
-  const { data } = useLocation(query);
-
-  return (
-    <div className="mt-8 mx-auto flex justify-center h-8 w-80">
-      <Combobox value={selected} onChange={setSelected}>
-        <Combobox.Input
-          className={"w-80  bg-white border border-black p-1 relative"}
-          onChange={(event) => setQuery(event.target.value)}
-          value={query}
-        />
-        <Combobox.Options
-          className={
-            "bg-white w-80 absolute my-9 border border-black border-t-0"
-          }
-        >
-          {data &&
-            data.map((address, index) => {
-              return (
-                <Combobox.Option
-                  className={"cursor-pointer max-w-80 px-1"}
-                  key={index}
-                  value={address.place_name}
-                >
-                  {address.place_name}
-                </Combobox.Option>
-              );
-            })}
-        </Combobox.Options>
-      </Combobox>
-    </div>
-  );
+	return (
+		<div className="mt-8 mx-auto flex justify-center h-8 w-80  bg-white border border-black">
+			<Combobox value={selected} onChange={setSelected}>
+				<Combobox.Input
+					placeholder="Location"
+					className={"w-80  p-1 relative"}
+					onChange={(event) => setQuery(event.target.value)}
+					value={query}
+				/>
+				<MapPinIcon
+					onClick={(event) => {
+						gps && setSelected(gps[0].place_name.toString());
+					}}
+					className="h-6 w-6"
+				/>
+				<Combobox.Options
+					className={
+						"bg-white w-80 absolute my-9 border border-black border-t-0"
+					}
+				>
+					{data &&
+						data.map((address, index) => {
+							return (
+								<Combobox.Option
+									className={"cursor-pointer max-w-80 px-1"}
+									key={index}
+									value={address.place_name}
+								>
+									{address.place_name}
+								</Combobox.Option>
+							);
+						})}
+				</Combobox.Options>
+			</Combobox>
+		</div>
+	);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,27 @@
 import Head from "next/head";
 import Button, { ButtonVariant } from "../components/button/button";
-import Test from "../components/test/test";
 import useProtectedPage from "../hooks/useProtectedPage";
 import LocationSearch from "../components/locationSearch/locationSearch";
-import { useState } from "react";
-import GPSToAddress from "../components/gpsToAddress/gpsToAddress";
 
 export default function Home() {
-  const authContext = useProtectedPage();
-  const [selected, setSelected] = useState("");
+	const authContext = useProtectedPage();
 
-  return (
-    <div>
-      <main>
-        <div>
-          <GPSToAddress selected={selected} setSelected={setSelected} />
-          <LocationSearch selected={selected} setSelected={setSelected} />
-          <div className="flex justify-center">
-            <div className="w-80">
-              <Button
-                label={"SEARCH"}
-                link={{ pathname: "/cards" }}
-                variant={ButtonVariant.fill}
-              />
-            </div>
-          </div>
-        </div>
-      </main>
-    </div>
-  );
+	return (
+		<div>
+			<main>
+				<div>
+					<LocationSearch />
+					<div className="flex justify-center">
+						<div className="w-80">
+							<Button
+								label={"SEARCH"}
+								link={{ pathname: "/cards" }}
+								variant={ButtonVariant.fill}
+							/>
+						</div>
+					</div>
+				</div>
+			</main>
+		</div>
+	);
 }
