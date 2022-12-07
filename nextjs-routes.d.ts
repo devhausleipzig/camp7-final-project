c3807133ae9ab51bae31fff0c8969923bd391a26
Dan McAtee <danielmcatee@me.com>
2022-12-07 12:00:02 +0100

    update routes

--- nextjs-routes.d.ts
+++ nextjs-routes.d.ts
@@ -14,9 +14,9 @@ declare module "nextjs-routes" {
     | { pathname: "/api/user"; query?: Query | undefined }
     | { pathname: "/api/user/me"; query?: Query | undefined }
     | { pathname: "/api/user/test"; query?: Query | undefined }
+    | { pathname: "/cards"; query?: Query | undefined }
     | { pathname: "/chat/[chat_id]"; query: Query<{ "chat_id": string }> }
     | { pathname: "/chat/example"; query?: Query | undefined }
-    | { pathname: "/cards"; query?: Query | undefined }
     | { pathname: "/chat"; query?: Query | undefined }
     | { pathname: "/"; query?: Query | undefined }
     | { pathname: "/login"; query?: Query | undefined }
