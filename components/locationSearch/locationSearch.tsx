febf1724110034484af61c73f1ed3cc4476a1494
Dan McAtee <danielmcatee@me.com>
2022-12-08 15:48:53 +0100

    review: refinement
    - made sime small ui tweaks
    - add option to clear the combobox
    - simplified distance data structure in select
    - made distanceelect absolute, so it doesn't interfer with the ui

--- components/locationSearch/locationSearch.tsx
+++ components/locationSearch/locationSearch.tsx
@@ -1,53 +1,58 @@
-import React, { useState } from "react";
 import { Combobox } from "@headlessui/react";
-import { useGpsLocation, useLocation } from "../../hooks/useLocation";
 import { MapPinIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
 import { DistanceFilter } from "../../components/distanceFilter/distanceFilter";
-import DistanceSelect from "../distanceFilter/distanceSelect";
+import { useGpsLocation, useLocation } from "../../hooks/useLocation";
 
 export default function LocationSearch() {
-	const [selected, setSelected] = useState("");
-	const { data: gps } = useGpsLocation();
-	const [query, setQuery] = useState("");
-	const { data } = useLocation(query);
+  const [selected, setSelected] = useState("");
+  const { data: gps } = useGpsLocation();
+  const [query, setQuery] = useState("");
+  const { data } = useLocation(query);
 
-	return (
-		<div>
-			<div className="mt-8 mx-auto flex justify-center h-8 w-80  bg-white border border-black">
-				<Combobox value={selected} onChange={setSelected}>
-					<MapPinIcon
-						onClick={(event) => {
-							gps && setSelected(gps[0].place_name.toString());
-						}}
-						className="h-6 w-6"
-					/>
-					<Combobox.Input
-						placeholder="Location"
-						className={"w-80  p-1 relative"}
-						onChange={(event) => setQuery(event.target.value)}
-						value={query}
-					/>
-					<Combobox.Options
-						className={
-							"bg-white w-80 absolute my-9 border border-black border-t-0"
-						}
-					>
-						{data &&
-							data.map((address, index) => {
-								return (
-									<Combobox.Option
-										className={"cursor-pointer max-w-80 px-1"}
-										key={index}
-										value={address.place_name}
-									>
-										{address.place_name}
-									</Combobox.Option>
-								);
-							})}
-					</Combobox.Options>
-				</Combobox>
-				<DistanceFilter />
-			</div>
-		</div>
-	);
+  return (
+    <div className="p-4">
+      <div className="mt-8 relative mx-auto flex justify-between bg-white border border-black">
+        <Combobox value={selected} onChange={setSelected}>
+          <MapPinIcon
+            onClick={() => {
+              gps && setSelected(gps[0].place_name.toString());
+            }}
+            className="h-8 w-8 p-1 bg-purple-700 text-white m-1 rounded-sm"
+          />
+          <Combobox.Input
+            placeholder="Location"
+            className={"px-1 flex-1 relative"}
+            onChange={(event) => setQuery(event.target.value)}
+            value={query}
+          />
+          <Combobox.Options
+            className={
+              "bg-white absolute my-9 border inset-x-0 border-black border-t-0"
+            }
+          >
+            <Combobox.Option
+              className={"cursor-pointer w-full px-1"}
+              value={""}
+            >
+              ❌ Clear
+            </Combobox.Option>
+            {data &&
+              data.map((address, index) => {
+                return (
+                  <Combobox.Option
+                    className={"cursor-pointer w-full px-1"}
+                    key={index}
+                    value={address.place_name}
+                  >
+                    {address.place_name}
+                  </Combobox.Option>
+                );
+              })}
+          </Combobox.Options>
+        </Combobox>
+        <DistanceFilter />
+      </div>
+    </div>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,26 @@
-import Head from "next/head";
 import Button, { ButtonVariant } from "../components/button/button";
-import useProtectedPage from "../hooks/useProtectedPage";
 import LocationSearch from "../components/locationSearch/locationSearch";
+import useProtectedPage from "../hooks/useProtectedPage";
 
 export default function Home() {
-	const authContext = useProtectedPage();
+  const authContext = useProtectedPage();
 
-	return (
-		<div>
-			<main>
-				<div>
-					<LocationSearch />
-					<div className="flex justify-center">
-						<div className="w-80">
-							<Button
-								label={"SEARCH"}
-								link={{ pathname: "/cards" }}
-								variant={ButtonVariant.fill}
-							/>
-						</div>
-					</div>
-				</div>
-			</main>
-		</div>
-	);
+  return (
+    <div>
+      <main>
+        <div>
+          <LocationSearch />
+          <div className="flex justify-center">
+            <div className="w-full mx-4">
+              <Button
+                label={"SEARCH"}
+                link={{ pathname: "/cards" }}
+                variant={ButtonVariant.fill}
+              />
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 }
