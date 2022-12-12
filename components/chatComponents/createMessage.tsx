ee4e82b382a6ffbeb5143a25be89353a9ae3af4e
Franz Wollang <franzwollang@gmail.com>
2022-12-12 16:18:38 +0100

    added react query

--- components/chatComponents/createMessage.tsx
+++ components/chatComponents/createMessage.tsx
@@ -1,69 +1,71 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { FormEvent, FormEventHandler, useState } from "react";
+import { methods } from "../../utils/methods";
 
 interface Props {
-  chat_id: string;
-  author_id: string;
+	chat_id: string;
+	author_id: string;
 }
 
-const initialMessage = {
-  name: "",
-  content: "",
-};
-
 export function CreateMessage({ chat_id, author_id }: Props) {
-  const [message, setMessage] = useState(initialMessage);
+	const [message, setMessage] = useState("");
+	const queryClient = useQueryClient();
 
-  function handleSubmit(event: FormEvent) {
-    event.preventDefault();
-    try {
-      const response = fetch(`http://localhost:3000/chat/${chat_id}`, {
-        method: "POST",
-        headers: {
-          "content-type": "application/json",
-        },
-        body: JSON.stringify({
-          content: message.content,
-          authorId: author_id,
-        }),
-      });
-      setMessage(initialMessage);
-    } catch (err) {
-      alert("Something went wrong");
-    }
-  }
+	const postMessage = useMutation({
+		mutationKey: ["conversations", chat_id],
+		mutationFn: async () => {
+			await fetch(`http://localhost:3000/api/chat/${chat_id}`, {
+				method: methods.post,
+				headers: {
+					"content-type": "application/json"
+				},
+				body: JSON.stringify({
+					content: message,
+					authorId: author_id
+				})
+			});
+			setMessage("");
+		}
+	});
 
-  return (
-    // <form onSubmit={handleSubmit} className="flex h-full bg-gray-500">
-    //   <input
-    //     value={message.message}
-    //     onChange={(event) =>
-    //       setMessage({ ...message, message: event.target.value })
-    //     }
-    //     name="message"
-    //     placeholder="type your message...."
-    //   />
-    //   <button className="bg-slate-700 p-2 text-slate-50" type="submit">
-    //     Send
-    //   </button>
-    // </form>
+	function handleSubmit(event: FormEvent) {
+		event.preventDefault();
+		postMessage.mutate();
+	}
 
-    <div className="flex absolute justify-center items-center bottom-0 w-full h-[8%]">
-      <div className="flex justify-items-center w-11/12 h-4/5 bg-blue-800 border-2 border-pink-600">
-        <form className="flex h-full bg-gray-500" onSubmit={handleSubmit}>
-          <div className="flex bottom-0 bg-black">
-            <input
-              value={message.content}
-              onChange={(event) =>
-                setMessage({ ...message, content: event.target.value })
-              }
-              placeholder="type your message...."
-            />
-          </div>
-          <button className="" type="submit">
-            Submit
-          </button>
-        </form>
-      </div>
-    </div>
-  );
+	return (
+		// <form onSubmit={handleSubmit} className="flex h-full bg-gray-500">
+		//   <input
+		//     value={message.message}
+		//     onChange={(event) =>
+		//       setMessage({ ...message, message: event.target.value })
+		//     }
+		//     name="message"
+		//     placeholder="type your message...."
+		//   />
+		//   <button className="bg-slate-700 p-2 text-slate-50" type="submit">
+		//     Send
+		//   </button>
+		// </form>
+
+		<div className="flex absolute justify-center items-center bottom-0 w-full h-[8%]">
+			<div className="flex justify-items-center w-11/12 h-4/5 bg-blue-800 border-2 border-pink-600">
+				<form
+					className="flex h-full bg-gray-500"
+					onSubmit={handleSubmit}
+				>
+					<div className="flex bottom-0 bg-black">
+						<input
+							value={message}
+							onChange={(event) => setMessage(event.target.value)}
+							placeholder="type your message...."
+						/>
+					</div>
+					<button className="" type="submit">
+						Submit
+					</button>
+				</form>
+			</div>
+		</div>
+	);
 }
--- pages/chat/[chat_id].tsx
+++ pages/chat/[chat_id].tsx
@@ -11,97 +11,97 @@ import { methods } from "../../utils/methods";
 import { CreateMessage } from "../../components/chatComponents/createMessage";
 
 async function GetMessages(
-  setMessages: Dispatch<SetStateAction<Conversation[]>>,
-  chat_id: string,
-  authContext: any
+	setMessages: Dispatch<SetStateAction<Conversation[]>>,
+	chat_id: string,
+	authContext: any
 ) {
-  const token = authContext.token;
-  const user = authContext.user;
+	const token = authContext.token;
+	const user = authContext.user;
 
-  const messageResponse = await fetch(
-    `http://localhost:3000/api/chat/${chat_id}/getMessages`,
-    {
-      method: methods.get,
-    }
-  );
-  const chatResponse = await fetch("http://localhost:3000/api/chat", {
-    method: methods.get,
-    headers: { Authorization: token },
-  });
-  const data = await messageResponse.json();
-  setMessages(data);
+	const messageResponse = await fetch(
+		`http://localhost:3000/api/chat/${chat_id}/getMessages`,
+		{
+			method: methods.get
+		}
+	);
+	const chatResponse = await fetch("http://localhost:3000/api/chat", {
+		method: methods.get,
+		headers: { Authorization: token }
+	});
+	const data = await messageResponse.json();
+	setMessages(data);
 }
 
 export default function ChatPage() {
-  const router = useRouter();
-  const { chat_id } = router.query;
-  const authContext = useProtectedPage();
-  const {
-    data: conversation,
-    isLoading,
-    isError,
-  } = useConversation(chat_id as string);
+	const router = useRouter();
+	const { chat_id } = router.query;
+	const authContext = useProtectedPage();
+	const {
+		data: conversation,
+		isLoading,
+		isError
+	} = useConversation(chat_id as string);
 
-  if (isError) return <p>Something went wrong</p>;
+	if (isError) return <p>Something went wrong</p>;
 
-  if (isLoading) return <p>Loading...</p>;
+	if (isLoading) return <p>Loading...</p>;
 
-  return (
-    <div className="w-screen h-screen">
-      <header className="flex items-center justify-between h-[9%] bg-[#603BAD] py-3 px-5">
-        <button
-          onClick={() => router.back()}
-          className="h-6 w-6 rounded-full flex items-center"
-        >
-          <BackButton className="h-6 w-6 rounded-full" />
-        </button>
-        <div className="flex items-center gap-4">
-          {getAvatar(
-            getOtherParticipant(
-              conversation.participant,
-              authContext.user.name
-            ) ?? {}
-          )}
-          <h2 className="text-white font-medium">
-            {getOtherParticipant(
-              conversation.participant,
-              authContext.user.name
-            )
-              ? getOtherParticipant(
-                  conversation.participant,
-                  authContext.user.name
-                )?.name
-              : "Unknown"}
-          </h2>
-        </div>
-        <div>
-          {getOtherParticipant(
-            conversation.participant,
-            authContext.user.name
-          )?.interests.map((interest) => (
-            <span key={interest.id}>{interest.name}</span>
-          ))}
-        </div>
-      </header>
-      <div className="flex flex-col gap-2 py-2 px-3 h-[92%] overflow-y-auto">
-        {conversation.messages.map((message) => (
-          <div
-            className={clsx(
-              "p-2 w-1/3 h-auto",
-              message.authorId === authContext.user.id
-                ? "bg-[#BCDCBF] h-auto w-auto self-end rounded-l-2xl rounded-br-2xl"
-                : "bg-[#BFB1DE] h-auto w-auto self-start rounded-r-2xl rounded-bl-2xl"
-            )}
-          >
-            {message.content}
-          </div>
-        ))}
-      </div>
-      <CreateMessage
-        chat_id={chat_id as string}
-        author_id={authContext.user.id}
-      />
-      {/* <div className="flex absolute justify-center items-center bottom-0 w-full h-[8%]">
+	return (
+		<div className="w-screen h-screen">
+			<header className="flex items-center justify-between h-[9%] bg-[#603BAD] py-3 px-5">
+				<button
+					onClick={() => router.back()}
+					className="h-6 w-6 rounded-full flex items-center"
+				>
+					<BackButton className="h-6 w-6 rounded-full" />
+				</button>
+				<div className="flex items-center gap-4">
+					{getAvatar(
+						getOtherParticipant(
+							conversation.participant,
+							authContext.user.name
+						) ?? {}
+					)}
+					<h2 className="text-white font-medium">
+						{getOtherParticipant(
+							conversation.participant,
+							authContext.user.name
+						)
+							? getOtherParticipant(
+									conversation.participant,
+									authContext.user.name
+							  )?.name
+							: "Unknown"}
+					</h2>
+				</div>
+				<div>
+					{getOtherParticipant(
+						conversation.participant,
+						authContext.user.name
+					)?.interests.map((interest) => (
+						<span key={interest.id}>{interest.name}</span>
+					))}
+				</div>
+			</header>
+			<div className="flex flex-col gap-2 py-2 px-3 h-[92%] overflow-y-auto">
+				{conversation.messages.map((message) => (
+					<div
+						className={clsx(
+							"p-2 w-1/3 h-auto",
+							message.authorId === authContext.user.id
+								? "bg-[#BCDCBF] h-auto w-auto self-end rounded-l-2xl rounded-br-2xl"
+								: "bg-[#BFB1DE] h-auto w-auto self-start rounded-r-2xl rounded-bl-2xl"
+						)}
+					>
+						{message.content}
+					</div>
+				))}
+			</div>
+			<CreateMessage
+				chat_id={chat_id as string}
+				author_id={authContext.user.id}
+			/>
+			{/* <div className="flex absolute justify-center items-center bottom-0 w-full h-[8%]">
         <div className="flex justify-items-center w-11/12 h-4/5 bg-blue-800 border-2 border-pink-600">
           
           <form className="flex h-full bg-gray-500" onSubmit={handleSubmit}>
@@ -119,6 +119,6 @@ export default function ChatPage() {
           </button>
         </div>
       </div> */}
-    </div>
-  );
+		</div>
+	);
 }
