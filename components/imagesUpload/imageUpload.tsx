956c6368ec2263b1370ae0bc431a0a4f32959ea6
Dan McAtee <danielmcatee@me.com>
2022-12-15 16:53:08 +0100

    disable upload button if no file

--- components/imagesUpload/imageUpload.tsx
+++ components/imagesUpload/imageUpload.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import storage from "../../firebaseConfig";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { useNotificationStore } from "../../stores/notificationStore";
+import clsx from "clsx";
 
 const ImageUpload = () => {
   const [file, setFile] = useState<File | null>(null);
@@ -122,11 +123,12 @@ const ImageUpload = () => {
       <br />
       <div className="flex justify-around">
         <button
-          className={
+          className={clsx(
+            "disabled:opacity-50",
             isActiveDelete
               ? "w-32 h-10 rounded-lg bg-white text-lg mb-4 text-purple border border-purple"
               : "w-32 h-10 rounded-lg bg-white text-lg mb-4 text-purple"
-          }
+          )}
           onClick={deleteButton}
           disabled={!file}
         >
@@ -135,13 +137,13 @@ const ImageUpload = () => {
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              stroke-width="1.5"
+              strokeWidth="1.5"
               stroke="currentColor"
               className="w-6 h-6 fill-purple text-white"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
               />
             </svg>
@@ -149,25 +151,27 @@ const ImageUpload = () => {
           </div>
         </button>
         <button
-          className={
+          className={clsx(
+            "disabled:opacity-50",
             isActiveUpload
               ? "w-32 h-10 rounded-lg bg-white text-lg mb-4 text-purple border border-purple"
               : "w-32 h-10 rounded-lg bg-white text-lg mb-4 text-purple"
-          }
+          )}
           onClick={uploadButton}
+          disabled={!file}
         >
           <div className="flex items-center justify-around mx-1">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              stroke-width="1.5"
+              strokeWidth="1.5"
               stroke="currentColor"
               className="w-6 h-6"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
               />
             </svg>
