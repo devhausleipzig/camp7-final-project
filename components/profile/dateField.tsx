73465269e5f47388819dbdd8153b2c13524a165b
Christie Reid <christiereid@protonmail.com>
2022-12-15 15:20:12 +0100

    add date picker for birthday

---
+++ components/profile/dateField.tsx
@@ -0,0 +1,81 @@
+export default function DateField() {
+	function generateArrayOfDays() {
+		const days = [];
+
+		for (let i = 1; i <= 31; i++) {
+			days.push(i);
+		}
+		return days;
+	}
+
+	function generateArrayOfMonths() {
+		const months = [];
+
+		for (let i = 1; i <= 12; i++) {
+			months.push(i);
+		}
+		return months;
+	}
+
+	function generateArrayOfYears() {
+		const max = new Date().getFullYear() - 17;
+		const years = [];
+
+		for (let i = 1930; i <= max; i++) {
+			years.push(i);
+		}
+		return years;
+	}
+
+	const maxYear = Number(generateArrayOfYears().slice(-1));
+
+	return (
+		<div className="pb-14">
+			<h3 className="text-purple text-xl font-quicksand pb-4 font-bold">
+				What's Your Date of Birth?
+			</h3>
+			<input
+				type="number"
+				placeholder="DD"
+				className="w-16 h-10 pl-1.5 text-center font-quicksand placeholder:font-quicksand placeholder-lightpurple placeholder:pl-3 outline-purple rounded-md border-lightpurple border text-purple"
+				min="1"
+				max="31"
+				list="days"
+				required
+			></input>
+			<datalist id="days">
+				{generateArrayOfDays().map((day) => (
+					<option value={day}></option>
+				))}
+			</datalist>
+			<input
+				type="number"
+				placeholder="MM"
+				className="w-16 h-10 pl-1.5 text-center font-quicksand placeholder:font-quicksand placeholder-lightpurple placeholder:pl-2 mx-4 outline-purple rounded-md border-lightpurple border text-purple"
+				min="1"
+				max="12"
+				list="months"
+				required
+			></input>
+			<datalist id="months">
+				{generateArrayOfMonths().map((month) => (
+					<option value={month}></option>
+				))}
+			</datalist>
+			<input
+				type="number"
+				placeholder="YYYY"
+				className="w-20 h-10 pl-1.5 text-center font-quicksand placeholder:font-quicksand placeholder-lightpurple placeholder:pl-3 outline-purple rounded-md border-lightpurple border text-purple"
+				min="1930"
+				max={maxYear}
+				list="years"
+				required
+			></input>
+			<datalist id="years">
+				{generateArrayOfYears().map((year) => (
+					<option value={Number(year)}></option>
+				))}
+			</datalist>
+		</div>
+	);
+}
--- pages/editProfile.tsx
+++ pages/editProfile.tsx
@@ -1,19 +1,21 @@
 import ImageUpload from "../components/imagesUpload/imageUpload";
+import DateField from "../components/profile/dateField";
 import DescriptionField from "../components/profile/descriptionField";
 import GenderField from "../components/profile/genderField";
 import LanguagesField from "../components/profile/languagesField";
 import NameField from "../components/profile/nameField";
 
 export default function Settings() {
-  return (
-    <div className="w-screen h-screen py-8 text-center bg-[#EEF6EF]">
-      <div className="mb-12 grid gap-8">
-        <ImageUpload />
-        <NameField />
-        <DescriptionField />
-        <GenderField />
-        <LanguagesField />
-      </div>
-    </div>
-  );
+	return (
+		<div className="w-screen h-screen py-8 text-center bg-[#EEF6EF]">
+			<div className="mb-12 grid gap-8">
+				<ImageUpload />
+				<NameField />
+				<DescriptionField />
+				<GenderField />
+				<LanguagesField />
+				<DateField />
+			</div>
+		</div>
+	);
 }
